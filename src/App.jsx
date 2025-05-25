f447d538fc11faa26aeb858adee444520e7cd9a7
nmcsakib <nmcsakib.1@gmail.com>
2025-05-25 23:47:17 +0600

    added time and submit section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import MCQContainer from "./components/mcqContainer/MCQContainer"
+import MCQContainer from "./Components/MCQContainer/MCQContainer"
 
 function App() {
 
   return (
-    <div className="container mx-auto text-center flex justify-center items-center gap-5 flex-col">
+  <div className="fullcontainer font-[Poppins] font-sembold" >
+      <div className="container mx-auto text-center flex justify-center items-center gap-5 flex-col">
     <h1 className="text-5xl text-white mt-8">Welcome</h1>
     <MCQContainer/>
     </div>
+  </div>
   )
 }
 
--- src/Components/MCQContainer/MCQContainer.jsx
+++ src/Components/MCQContainer/MCQContainer.jsx
@@ -1,6 +1,21 @@
+import { FaClock, FaRegClock } from "react-icons/fa";
+
 const MCQContainer = () => {
     return (
-        <div className="rounded-xl w-[1000px] border h-[500px]">
+        <div className="rounded-xl w-[1000px] border h-[500px] bg-white">
+            {/* Time and submit */}
+           <div className="flex justify-between items-center p-5">
+             <div className="text-cyan-600 flex items-center gap-3">
+                <FaRegClock className="text-3xl"/> 
+                <div className="text-left leading-relaxed text-sm">
+                    <p className="text-xs text-gray-500">Time remaining</p>
+                    <p>00:02:43 seconds</p>
+                </div>
+            </div>
+            <button className="btn btn-success">Submit</button>
+           </div>
+            {/* Time and submit */}
+
             
         </div>
     );
