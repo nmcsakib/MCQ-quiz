8e8932affebd75d1358d2303b840b30aa3f00b27
nmcsakib <nmcsakib.1@gmail.com>
2025-05-31 23:26:44 +0600

    added time format

--- src/utility/addToLocal.js
+++ src/utility/addToLocal.js
@@ -4,9 +4,17 @@ const getData = () => {
 };
 
 const addToLocal = (submittedScore) => {
+    const now = new Date();
+
+const year = now.getFullYear();         // Year (e.g., 2025)
+const month = now.getMonth() + 1;       // Month (0-11, so add 1)
+const day = now.getDate();              // Day of the month (1-31)
+const hour = now.getHours();            // Hours (0-23)
+const minute = now.getMinutes();        // Minutes (0-59)
+const second = now.getSeconds(); 
     const newEntry = {
         score: submittedScore,
-        time: new Date().toISOString() 
+        time:  `${year}-${month}-${day} ${hour}h-${minute}min-${second}sec`
     };
 
     const storedList = getData();
